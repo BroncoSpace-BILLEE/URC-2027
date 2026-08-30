a981930018550d6373e7e925b5638a5f2a4f1db0
Shelby Howard <shelbyyhowardd@gmail.com>
2026-08-29 21:55:45 -0700

    Move colcon shell tooling into Pixi

--- ros2_ws/colcon_setup.sh
+++ ros2_ws/colcon_setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# The Pixi colcon-shell task uses this file as an interactive Bash startup file.
+if [ "${COLCON_SETUP_SOURCE_BASHRC:-0}" = "1" ]; then
+  unset COLCON_SETUP_SOURCE_BASHRC
+
+  if [ -f "${HOME}/.bashrc" ]; then
+    source "${HOME}/.bashrc"
+  fi
+fi
+
 if [ -f "${CONDA_PREFIX}/share/colcon_argcomplete/hook/colcon-argcomplete.bash" ]; then
   source "${CONDA_PREFIX}/share/colcon_argcomplete/hook/colcon-argcomplete.bash"
 fi
